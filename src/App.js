ebd8ad69b45a77d35f857b5c56c6d4cdbdda55f2
Hemant <hrsmart2150@gmail.com>
2023-05-06 15:07:21 +0530

    Drag-n-Drop blog post example code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 import './App.css';
 
-function App() {
+const initialItems = [
+  { id: '1', content: 'Item 1' },
+  { id: '2', content: 'Item 2' },
+  { id: '3', content: 'Item 3' },
+];
+
+const App = () => {
+  const [items, setItems] = useState(initialItems);
+
+  const onDragEnd = (result) => {
+    if (!result.destination) return;
+    const { source, destination } = result;
+    const newItems = [...items];
+    const [removed] = newItems.splice(source.index, 1);
+    newItems.splice(destination.index, 0, removed);
+    setItems(newItems);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <DragDropContext onDragEnd={onDragEnd}>
+      <Droppable droppableId="items">
+        {(provided) => (
+          <ul {...provided.droppableProps} ref={provided.innerRef}>
+            {items.map((item, index) => (
+              <Draggable key={item.id} draggableId={item.id} index={index}>
+                {(provided, snapshot) => (
+                  // Inside the Draggable component:
+                  <li
+                    ref={provided.innerRef}
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                    style={{
+                      backgroundColor: snapshot.isDragging ? 'rgb(231, 179, 179)' : '',
+                      ...provided.draggableProps.style
+                    }}
+                    className='item'
+                  >
+                    {item.content}
+                  </li>
+                )}
+              </Draggable>
+            ))}
+            {provided.placeholder}
+          </ul>
+        )}
+      </Droppable>
+    </DragDropContext>
   );
-}
+};
 
 export default App;
